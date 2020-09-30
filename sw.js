80155457a3fd5f9db6333b786dc833cea9937419
javalo <lo.cheikhna@gmail.com>
2020-10-01 01:30:11 +0200

    manage the cache version and dynamic cache

--- sw.js
+++ sw.js
@@ -1,14 +1,64 @@
 
-self.addEventListener('install', evt => {
-    console.log("service Worker has been installed ");
-});
+const staticCacheName = 'site-static-v2';
+const dynamicChache = 'site-danymic-v1'
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/salade.jpg',
+  '/img/chicken.png',
+  '/img/dinner.jpg',
+  '/img/Hamburger.jpg',
+  '/img/meat.png',
+  '/img/pancakes.png',
+  '/img/pizza.jpeg',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'];
 
-// listener is Activated
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+      })
+    );
+  });
+
+// listener  Activated
 self.addEventListener('activate', evt=>{
-    console.log('service worker has been activated ');
+   // console.log('service worker has been activated ');
+
+   evt.waitUntil(
+       caches.keys().then(keys => {
+        console.log(keys);
+           return Promise.all(keys
+            .filter(key => key !== staticCacheName)
+            .map(key => caches.delete(key))
+            )
+       })
+   );
 });
 // fetch event 
 
 self.addEventListener('fetch', evet =>{
-    console.log('fetch event ', evet);
+    // console.log('fetch event ', evet);
+    evet.respondWith(
+        caches.match(evet.request).then(cachesRes=>{
+            return cachesRes || fetch(evet.request).then(
+                fetchRes => {
+                    return caches.open(dynamicChache).then(
+                        cache => {
+                            cache.put(evet.request.url, fetchRes.clone());
+                            return fetchRes;
+                        }
+                    )
+                }
+            );
+        })
+    )
+    
 });
